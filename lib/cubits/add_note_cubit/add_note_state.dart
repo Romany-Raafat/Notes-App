0e78ebd93d29e1d9040e216d81f30de61ec3c986
Romany-Raafat <romanyraafat96@gmail.com>
2025-10-30 23:57:04 +0200

    add notes states

--- lib/cubits/add_note_cubit/add_note_state.dart
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -1,1 +1,12 @@
 class AddNoteState {}
+
+class AddNoteLoading extends AddNoteState {}
+
+class AddNoteInitial extends AddNoteState {}
+
+class AddNoteLoaded extends AddNoteState {}
+
+class AddNoteFailure extends AddNoteState {
+  final String errorMessage;
+  AddNoteFailure(this.errorMessage);
+}
